767b750ec75271ad55004ee1cc137b677e7501b9
YunSwain <destap@naver.com>
2024-05-14 16:01:13 +0900

    chore: json 프로퍼티명 Swiftable 하게 수정

--- FootprintsOfKorea/Infrastructure/Network/Entities/KeywordSearchResponseModel.swift
+++ FootprintsOfKorea/Infrastructure/Network/Entities/KeywordSearchResponseModel.swift
@@ -1,8 +1,63 @@
-//
-//  KeywordSearchResponseModel.swift
-//  FootprintsOfKorea
-//
-//  Created by Swain Yun on 5/13/24.
-//
-
 import Foundation
+
+struct KeyworkSearchReponseModel: Decodable {
+    let response: Response
+}
+
+struct Response: Decodable {
+    let header: Header
+    let body: Body
+}
+
+struct Header: Decodable {
+    let resultCode, resultMessage: String
+    
+    enum CodingKeys: String, CodingKey {
+        case resultCode
+        case resultMessage = "resultMsg"
+    }
+}
+
+struct Body: Decodable {
+    let items: Items
+    let numberOfRows, pageNumber, totalCount: Int
+    
+    enum CodingKeys: String, CodingKey {
+        case items, totalCount
+        case numberOfRows = "numOfRows"
+        case pageNumber = "pageNo"
+    }
+}
+
+struct Items: Decodable {
+    let item: [Item]
+}
+
+struct Item: Decodable {
+    let address, detailAddress, areacode, booktour: String
+    let categoryFirst, categorySecond, categoryThird, contentId: String
+    let contentTypeId, createdTime: String
+    let primaryImage, secondaryImage: String
+    let copyrightDivisionCode, coordinateX, coordinateY, mapLevel: String
+    let modifiedTime, sigungucode, telephoneNumber, title: String
+
+    enum CodingKeys: String, CodingKey {
+        case areacode, booktour, title, sigungucode
+        case address = "addr1"
+        case detailAddress = "addr2"
+        case categoryFirst = "cat1"
+        case categorySecond = "cat2"
+        case categoryThird = "cat3"
+        case contentId = "contentid"
+        case contentTypeId = "contenttypeid"
+        case createdTime = "createdtime"
+        case primaryImage = "firstimage"
+        case secondaryImage = "firstimage2"
+        case copyrightDivisionCode = "cpyrhtDivCd"
+        case coordinateX = "mapx"
+        case coordinateY = "mapy"
+        case mapLevel = "mlevel"
+        case modifiedTime = "modifiedtime"
+        case telephoneNumber = "tel"
+    }
+}
