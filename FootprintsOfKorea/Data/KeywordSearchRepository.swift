f07db325d8076ce1cef74514abb3575f590390f3
YunSwain <destap@naver.com>
2024-05-15 18:09:38 +0900

    refactor: APIKey 접근은 Endpoint가 알아서 하도록 코드 이동

--- FootprintsOfKorea/Data/KeywordSearchRepository.swift
+++ FootprintsOfKorea/Data/KeywordSearchRepository.swift
@@ -5,12 +5,10 @@ protocol KeywordSearchRepository {
 }
 
 enum KeywordSearchRepositoryError: Error, CustomDebugStringConvertible {
-    case invalidKey
     case fetchFailed
     
     var debugDescription: String {
         switch self {
-        case .invalidKey: "키가 유효하지 않음"
         case .fetchFailed: "값을 가져올 수 없음"
         }
     }
@@ -26,11 +24,6 @@ final class DefaultKeywordSearchRepository {
         self.networkService = networkService
     }
     
-    private func validateAPIKey() throws -> Key {
-        guard let key = apiKey else { throw KeywordSearchRepositoryError.invalidKey }
-        return key
-    }
-    
     private func mapToBasicModel(with entity: KeywordSearchResponseModel) -> [BasicModel] {
         return entity.response.body.items.item.map { $0.toDTO() }
     }
@@ -40,11 +33,9 @@ final class DefaultKeywordSearchRepository {
 extension DefaultKeywordSearchRepository: KeywordSearchRepository {
     func search(by keyword: String) async throws -> [BasicModel] {
         do {
-            let apiKey = try validateAPIKey()
             let query = Endpoint.searchKeyword(osType: .ios,
                                                arrangeType: .byModifiedTime,
-                                               keyword: keyword,
-                                               key: apiKey)
+                                               keyword: keyword)
             let model = try await networkService.request(query: query, for: KeywordSearchResponseModel.self)
             return mapToBasicModel(with: model)
         } catch let error {
